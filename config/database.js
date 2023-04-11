155f2d341a8b55d33f1c59a1ecc415b53961b760
zisam-glitch <128255158+zisam-glitch@users.noreply.github.com>
2023-04-11 22:48:37 +0600

    initial commit

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = ({ env }) => {
+
+  const connections = {
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          '..',
+          env('DATABASE_FILENAME', 'data.db')
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+};
